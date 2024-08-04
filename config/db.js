91d4e914d8dc08285b61b1cd2ea993fc99948942
= <chinthadakirankumar@gmail.com>
2024-08-04 16:43:37 +0530

    change db connection

--- config/db.js
+++ config/db.js
@@ -1,12 +1,13 @@
 // db.js
 const mysql = require('mysql2');
+const dotenv = require("dotenv").config();
 
 // Create a connection to the database
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root', // replace with your MySQL username
-    password: '', // replace with your MySQL password
-    database: 'snab'
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER, // replace with your MySQL username
+    password: process.env.MYSQL_PASSWORD, // replace with your MySQL password
+    database: process.env.MYSQL_DATABASE
 });
 
 // Connect to the database
